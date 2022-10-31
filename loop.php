55c2a44866e9477bf2c082ed8dde1a7f08f2609b
ainulfurkan <m.ainulfurkan@gmail.com>
2022-10-31 03:57:13 +0000

    Initial commit

---
+++ loop.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+ <h1>loop php</h1>
+<?php
+  
+  //for($i=0; $i<5; $i ++)
+  {
+   //echo "--------------";
+   //echo "sekolah koding $1";
+     //echo "-------------<br>";
+  
+  }
+
+$hewan = ['anjing','babi','cicak', 'domba'];
+ for ($i=0 ;  )
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  ?>
+ <!--
+  This script places a badge on your repl's full-browser view back to your repl's cover
+  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+  teal, blue, blurple, magenta, pink!
+  -->
+  <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+</body>
+
+</html>
